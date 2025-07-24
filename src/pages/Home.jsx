a24da875a201bf769ebf543958e44b5827619c1c
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-24 15:09:17 -0400

    added some simple pages and tried Router in App.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is your main landing page.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/ItemsListingsForSale.jsx
@@ -0,0 +1,13 @@
+
+
+
+function ItemsListingsForSale() {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      <p>This is the content of Page 1.</p>
+    </div>
+  );
+}
+
+export default ItemsListingsForSale;
