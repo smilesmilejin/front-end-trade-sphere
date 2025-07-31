843b4531fa0cbffdb41b1db9a47c12871e889df9
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-31 16:01:41 -0400

    refactor Item -delete comments

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,9 @@
 import PropTypes from 'prop-types';
-import '../styles/Item.css';
-
-import ImageGallerySlider from './ImageGallerySlider.jsx';
 import { useState } from 'react';
 import NoImageAvailable from '../assets/NoImageAvailable.png';
-
+import ImageGallerySlider from './ImageGallerySlider.jsx';
 import EditUserItemForm from './EditUserItemForm.jsx';
-import ImageList from './ImageList.jsx';
+import '../styles/Item.css';
 
 const sampleImagesArray = [
     "https://i.ibb.co/gLQ48Lrp/71-O-3j3d9-DL-AC-SL1500.jpg",
@@ -14,10 +11,6 @@ const sampleImagesArray = [
     "https://i.ibb.co/jZwGPvXP/premium-photo-1710346961272-c23f174b6e39.jpg",
     "https://i.ibb.co/DDMbKGDp/indoor-photo-realistic-green-christmas-260nw-2539815647-jpg.webp",
 ]
-
-
-
-// const sampleImagesArray = []
 
 
 const Item =({
@@ -50,39 +43,18 @@ const Item =({
     const handleEditOpen = () => setIsEditOpen(true);
     const handleEditClose = () => setIsEditOpen(false);
 
-    // const imageUrls = images.map(image => image.image_url);
-
     const imageUrls = images?.map(image => image.image_url) || [];
-    // const isLiked = userLikedListings.has(listing_id);
     const isLiked = (userLikedListings || new Set()).has(listing_id);
 
     return (
         <li>
-            {/* <ImageGallery 
-                // images={images}
-                images={sampleImagesArray}
-            /> */}
             <ImageGallerySlider 
-                // images={images}
-                // images={sampleImagesArray}
-                // images={images && images.length > 0 ? images : [NoImageAvailable]}
                 images={imageUrls && imageUrls.length > 0 ? imageUrls : [NoImageAvailable]}
-
             />
-            {/* <p>Listing Id: {listing_id}</p>
-            <p>User Id: {user_id}</p> */}
             <p>Name: {name}</p>
             <p>Category: {category}</p>
-            {/* <p>Description: {description}</p> */}
             <p>Price: {price}</p>
-            {/* <p>Location: {location}</p>
-            <p>Contact Information: {contact_information}</p>
-            <p>Created at : {created_at}</p>
-            <p>Updated At: {updated_at}</p> */}
 
-            {/* When sold_status is false, React renders nothing, because false is a falsy value and not rendered as a string by default in JSX. */}
-            {/* <p>Sold Status: {sold_status ? 'Sold' : 'Available'}</p> */}
-            
             {/* Heart icon */}
             <button onClick={() => onToggleLike(listing_id)}>
             {isLiked ? '❤️' : '🤍'}
@@ -95,7 +67,7 @@ const Item =({
             </div>
 
             <div onClick={() => onDeleteUserItem(listing_id)}>
-            {deleteButton}
+                {deleteButton}
             </div>
 
             {isModalOpen && (
@@ -103,14 +75,11 @@ const Item =({
                     <div className="modal">
                         <button className="close-button" onClick={handleClose}>×</button>
                         <h2>Listing Details</h2>
-                        {/* <ImageGallerySlider 
-                            // images={images}
-                            images={sampleImagesArray}
-                        /> */}
+
                         <ImageGallerySlider 
-                            // images={images && images.length > 0 ? images : [NoImageAvailable]}
                             images={imageUrls && imageUrls.length > 0 ? imageUrls : [NoImageAvailable]}
                         />
+
                         <p>Listing Id: {listing_id}</p>
                         <p>User Id: {user_id}</p>
                         <p>Name: {name}</p>
@@ -131,21 +100,6 @@ const Item =({
                     <div className="modal">
                         <button className="close-button" onClick={handleEditClose}>×</button>
                         <h2>Listing Details</h2>
-                        {/* <ImageGallerySlider 
-                            // images={images && images.length > 0 ? images : [NoImageAvailable]}
-                            images={imageUrls && imageUrls.length > 0 ? imageUrls : [NoImageAvailable]}
-                        /> */}
-                        {/* <p>Listing Id: {listing_id}</p>
-                        <p>User Id: {user_id}</p>
-                        <p>Name: {name}</p>
-                        <p>Category: {category}</p>
-                        <p>Description: {description}</p>
-                        <p>Price: ${price}</p>
-                        <p>Location: {location}</p>
-                        <p>Contact Info: {contact_information}</p>
-                        <p>Created At: {created_at}</p>
-                        <p>Updated At: {updated_at}</p>
-                        <p>Status: {sold_status ? 'Sold' : 'Available'}</p> */}
 
                         <EditUserItemForm
                         itemData={{
@@ -198,22 +152,5 @@ Item.propTypes = {
     onDeleteImage: PropTypes.func,
 }
 
-
-// Item.propTypes = {
-//   listing_id: PropTypes.number.isRequired,
-//   user_id: PropTypes.number.isRequired,
-//   name: PropTypes.string,
-//   category: PropTypes.string,
-//   description: PropTypes.string,
-//   price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-//   location: PropTypes.string,
-//   contact_information: PropTypes.string,
-//   created_at: PropTypes.string.isRequired,
-//   updated_at: PropTypes.string.isRequired,
-//   sold_status: PropTypes.bool.isRequired,
-//   images: PropTypes.array.isRequired,
-//   userLikedListings: PropTypes.instanceOf(Set),
-//   onToggleLike: PropTypes.func,
-// };
 
 export default Item;
