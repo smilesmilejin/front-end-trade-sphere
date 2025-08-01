ae2b386cdb49527094480202efc7350b4f4b85ae
Xinshuang Jin <jinxinshuang@gmail.com>
2025-08-01 00:19:14 -0400

    refactor ProfilePostItem

--- src/pages/ProfilePostItem.jsx
+++ src/pages/ProfilePostItem.jsx
@@ -1,13 +1,11 @@
-// PostItem.jsx
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import NewItemForm from '../components/NewItemForm';
 import UserContext from '../contexts/UserContext';
 import axios from 'axios';
 
 // get backendUrl from .env file
 const kBaseUrl = import.meta.env.VITE_APP_BACKEND_URL;
-console.log("In ItemsForSale page, the kBaseUrl:", kBaseUrl);
-
+// console.log("In ItemsForSale page, the kBaseUrl:", kBaseUrl);
 
 // post new Listing to the database
 const postItemApi = (userId, newItemData) => {
@@ -24,27 +22,18 @@ const postItemApi = (userId, newItemData) => {
 
 function ProfilePostItem() {
   const { curUserData } = useContext(UserContext);
-
-  console.log('user Context is now: ', curUserData);
-
-  // const [successMessage, setSuccessMessage] = useState('');
-  // const [errorMessage, setErrorMessage] = useState('');
-
+  // console.log('user Context is now: ', curUserData);
   const curUserId = curUserData.user_id;
 
   const postItem= (newItemData) => {
     postItemApi(curUserId, newItemData)
     .then(response => {
-      // setSuccessMessage('✅ Your item was posted successfully!');
       alert('✅ Your item was posted successfully!')
-      // setErrorMessage('');
       return response.data;
     })
     .catch (error => {
       console.log(error);
       alert('❌ Failed to post the item. Please try again.')
-      // setErrorMessage('❌ Failed to post the item. Please try again.');
-      // setSuccessMessage('');
     });
   }
 
@@ -53,13 +42,6 @@ function ProfilePostItem() {
     <div>
       <h3>Post Item to Sell</h3>
       <p>Post a new item for sale here.</p>
-      {/* Show success or error message */}
-      {/* {successMessage && (
-        <div className="success-message">{successMessage}</div>
-      )}
-      {errorMessage && (
-        <div className="error-message">{errorMessage}</div>
-      )} */}
 
       <NewItemForm onPostItem={postItem}/>
 
