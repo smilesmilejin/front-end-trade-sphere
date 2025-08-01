dbf20451139830ab7966ed9f873630be09d2b777
Xinshuang Jin <jinxinshuang@gmail.com>
2025-08-01 17:50:10 -0400

    refactor Home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
+import '../styles/Home.css';
+
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to TradeSphare!</h1>
       <section>
-        <p>
-        Your smart marketplace to buy, sell, and connect with ease.
-        Discover great deals in Items for Sale in Charlotte NC!
-        Happy trading!
-        </p>
+        <p>Your smart marketplace to buy, sell, and connect with ease.</p>
+        <p>Discover great deals in Items for Sale in Charlotte NC!</p>
+        <p>Happy trading!</p>
       </section>
     </div>
   );
