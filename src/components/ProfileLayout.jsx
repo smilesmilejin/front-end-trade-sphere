9816202343d92393609218f2ed38efaa15fb38f5
Xinshuang Jin <jinxinshuang@gmail.com>
2025-08-04 18:50:00 -0400

    added Styles in ProfileLayout and make AccountNavBar and main content line up side by side

--- src/components/ProfileLayout.jsx
+++ src/components/ProfileLayout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router';
 import AccountNavbar from './AccountNavbar';
 import AccountHeader from './AccountHeader';
+import '../styles/ProfileLayout.css';
+
 
 function ProfileLayout() {
   return (
     <div className="profile-container">
       <AccountHeader />
-      <AccountNavbar />
-      <div className="profile-card">
-        <Outlet />
+      <div className="profile-layout">
+        <AccountNavbar />
+        <div className="profile-card">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
