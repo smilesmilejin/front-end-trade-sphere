535c82444e979b53e2cadd21235e8f6b7ef9a53b
Xinshuang Jin <jinxinshuang@gmail.com>
2025-08-07 22:35:23 -0400

    hide listingID and imageID in Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -8,8 +8,8 @@ const Image =({ image_id, listing_id, image_url, onLocalHandlelDeleteImage}) => 
             <div className="edit-image-wrapper">
                 <img src={image_url} alt={`Image ${image_id}`} />
             </div>
-            <p>Image ID: {image_id}</p>
-            <p>Listing ID: {listing_id}</p>
+            {/* <p>Image ID: {image_id}</p>
+            <p>Listing ID: {listing_id}</p> */}
             <button onClick={() => onLocalHandlelDeleteImage(image_id, listing_id)}>Delete Image</button>
         </li>
     )
